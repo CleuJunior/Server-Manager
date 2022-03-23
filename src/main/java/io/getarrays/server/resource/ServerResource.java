334e39a0f7a06d57b866fe48290ab1c369a38569
CleuJunior <cleu.junior@gmail.com>
2022-03-22 22:52:27 -0300

    Swagger Doc API

--- src/main/java/io/getarrays/server/resource/ServerResource.java
+++ src/main/java/io/getarrays/server/resource/ServerResource.java
@@ -1,9 +1,10 @@
 package io.getarrays.server.resource;
 
-import io.getarrays.server.enumeration.Status;
 import io.getarrays.server.model.Response;
 import io.getarrays.server.model.Server;
 import io.getarrays.server.service.implementations.ServerServiceImpl;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 
 import static io.getarrays.server.enumeration.Status.SERVER_UP;
@@ -11,13 +12,14 @@ import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.OK;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 
 import javax.validation.Valid;
@@ -31,11 +33,14 @@ import static org.springframework.http.MediaType.IMAGE_PNG_VALUE;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v1/server")
+@RequestMapping(value="/api")
+@Api(value="API Rest Server")
+@CrossOrigin(origins = "*")
 public class ServerResource {
     private final ServerServiceImpl serverService;
 
     @GetMapping("/list")
+    @ApiOperation(value = "Lista todos os servidores da API")
     public ResponseEntity<Response> getServers() {
         return ResponseEntity.ok(
                 Response
@@ -51,6 +56,7 @@ public class ServerResource {
     }
 
     @GetMapping("/ping/{ipAddress}")
+    @ApiOperation(value = "Ping no indereço IP")
     public ResponseEntity<Response> pingServer(@PathVariable("ipAddress") String ipAddress) throws IOException {
         Server server = serverService.ping(ipAddress);
         return ResponseEntity.ok(
@@ -67,6 +73,7 @@ public class ServerResource {
     }
 
     @PostMapping("/save")
+    @ApiOperation(value = "Salva um servidor")
     public ResponseEntity<Response> saveServer(@RequestBody @Valid Server server) {
         return ResponseEntity.ok(
                 Response
@@ -82,6 +89,7 @@ public class ServerResource {
     }
 
     @GetMapping("/get/{id}")
+    @ApiOperation(value = "Retornar um servidor pelo ID")
     public ResponseEntity<Response> getServer(@PathVariable("id") Long id) {
         return ResponseEntity.ok(
                 Response
@@ -97,6 +105,7 @@ public class ServerResource {
     }
 
     @DeleteMapping("/delete/{id}")
+    @ApiOperation(value = "Deleta um servido pelo número do ID")
     public ResponseEntity<Response> deleteServer(@PathVariable("id") Long id) {
         return ResponseEntity.ok(
                 Response
@@ -112,6 +121,7 @@ public class ServerResource {
     }
 
     @GetMapping(path = "/image/{fileName}", produces = IMAGE_PNG_VALUE)
+    @ApiOperation(value = "Retornar um imagem(png) de um servidor")
     public byte[] getServerImage(@PathVariable("fileName") String fileName) throws IOException {
         return Files.readAllBytes(Paths.get(System.getProperty("user.home") + "/Downloads/images/" + fileName));
 
